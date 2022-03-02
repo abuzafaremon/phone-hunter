8a0d438e41ed9371f4af5d5982845ab1540f83eb
zafaremon <abuzaforemon@gmail.com>
2022-03-02 12:53:53 +0600

    display phones in UI

--- js/script.js
+++ js/script.js
@@ -6,9 +6,24 @@ document.getElementById('search-btn').addEventListener('click', function () {
   const loadPhones = () => {
     fetch(phonesUrl)
       .then(res => res.json())
-      .then(data => console.log(data.data));
+      .then(data => displayPhones(data.data));
   }
   loadPhones();
 
-
+  const displayPhones = phones => {
+    const phonesDiv = document.getElementById('phones-div');
+    for (const phone of phones) {
+      const div = document.createElement('div');
+      div.classList.add('card');
+      div.innerHTML = `
+      <img src="${phone.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${phone.phone_name}</h5>
+        <p class="card-text">Brand: ${phone.brand}</p>
+        <a href="#" class="btn btn-success" >More Info</a>
+      </div>
+      `;
+      phonesDiv.appendChild(div);
+    }
+  }
 })
